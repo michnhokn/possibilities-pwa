040f3880cea4b33523f4581bdeb84d4ff7711a15
Michael Scheurich <michael_scheurich@web.de>
2019-07-05 10:47:42 +0200

    add FeatherIcons component

---
+++ src/components/FeatherIcon.vue
@@ -0,0 +1,36 @@
+<template>
+    <svg>
+        <use :xlink:href="iconPath"></use>
+    </svg>
+</template>
+
+<script>
+    import feather from 'feather-icons/dist/feather-sprite.svg';
+
+    export default {
+        name: "FeatherIcon",
+        props: {
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        computed: {
+            iconPath() {
+                return feather + '#' + this.name;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    svg {
+        width: 20px;
+        height: 20px;
+        fill: none;
+        stroke: currentColor;
+        stroke-width: 2px;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+    }
+</style>
