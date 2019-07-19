7cc56a6b7a1f84183dcf0427a4ab29d7a9577145
Michael Scheurich <michael_scheurich@web.de>
2019-07-19 13:59:12 +0200

    compile dev with icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
             <router-link to="/" tag="span">{{ title }}</router-link>
             {{ getRouteName() }}
         </div>
-        <p class="header__version">0.1.14</p>
+        <p class="header__version">0.1.15</p>
     </header>
 </template>
 
